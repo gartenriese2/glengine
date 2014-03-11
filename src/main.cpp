9a73eb50a46d7e0ecc0f9688ecaf2c30cf6fd5bf
gartenriese <gartenriese@gulli.com>
2014-03-11 01:30:34 +0100

    demo

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
 	WindowID w1 = e.createWindow(800, 600);
 	ObjectID t1 = w1.createTriangle({-0.5f, -0.5f, 0.f}, {0.5f, -0.5f, 0.f}, {0.f, 0.5f, 0.f});
 	ObjectID t2 = w1.createTriangle({-2.f, -2.f, 1.f}, {-1.5f, -2.f, 1.f}, {-1.75f, -1.5f, 1.f}, {1.f, 1.f, 0.f});
-	w1.createCopy(t2);
+	ObjectID t3 = w1.createCopy(t2);
 	t2.attachTo(t1);
 	t1.moveTo({1.5f, 0.f, 0.f});
 	t2.moveTo({1.f, -0.5f, 0.f});
@@ -23,6 +23,7 @@ int main() {
 
 	float step = 0.001f;
 	float rotPerSecond = 0.33;
+	long count = 0;
 	while(1) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(1000 * step)));
 		t1.rotateAround(6.28f * rotPerSecond * step, {0.f, 0.f, 1.f}, {0.f, 0.f, 0.f});
@@ -30,6 +31,9 @@ int main() {
 		t2.rotate(6.28f * 2*rotPerSecond * step, {0.f, 0.f, 1.f});
 		t1.scale({0.9999f, 0.9999f, 0.9999f});
 		q2.rotate(6.28f * rotPerSecond * step, {0.f, 0.f, 1.f});
+		if (count % 1000 == 200) t3.move(2.f, {1.f, 0.f, 0.f});
+		if (count % 1000 == 800) t3.move(2.f, {-1.f, 0.f, 0.f});
+		++count;
 	}
 
 	return 0;
